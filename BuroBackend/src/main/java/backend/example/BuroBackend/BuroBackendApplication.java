fac85387220f8472bf45de9ca32a91c7cfe7babe
jouxjr7 <joureyescr7@gmail.com>
2023-10-20 22:12:20 -0500

    init-1

---
+++ BuroBackend/src/main/java/backend/example/BuroBackend/BuroBackendApplication.java
@@ -0,0 +1,13 @@
+package backend.example.BuroBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuroBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuroBackendApplication.class, args);
+	}
+
+}
---
+++ BuroBackend/src/test/java/backend/example/BuroBackend/BuroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.example.BuroBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuroBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
